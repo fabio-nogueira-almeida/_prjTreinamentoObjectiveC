fc297ab508837dcbb8848dc7972e15ea71518bd0
Fábio Nogueira <fabio.nogueira@me.com>
2012-07-15 21:13:11 -0300

    Alguns exemplos de codigos para OBJECTIVE-C
    Contém exemplos de estruturas basicas

---
+++ _prjTreinamentoObjectiveC/main3.m
@@ -0,0 +1,13 @@
+//
+//  declarando Variaveis.m
+//  _prjTreinamentoObjectiveC
+//
+//  Created by Fábio Nogueira de Almeida on 14/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "declarando Variaveis.h"
+
+@implementation declarando_Variaveis
+
+@end
---
+++ main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  _prjTreinamentoObjectiveC
+//
+//  Created by Fábio Nogueira de Almeida on 14/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "main.h"
+
+@implementation main
+
+@end
